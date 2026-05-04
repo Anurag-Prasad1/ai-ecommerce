54772c6c6e6c2f0ef62bfb7f505f324954479f1f
Anurag Prasad <anuragprasad309@gmail.com>
2026-05-04 23:33:28 +0530

    feat: added product controller with create and get APIs

---
+++ backend/controllers/productController.js
@@ -0,0 +1,34 @@
+const Product = require("../models/Product");
+
+
+// @desc    Create Product
+// @route   POST /api/products
+// @access  Private
+const createProduct = async (req, res) => {
+  const { name, price, description, image, countInStock } = req.body;
+
+  const product = await Product.create({
+    name,
+    price,
+    description,
+    image,
+    countInStock,
+  });
+
+  res.status(201).json(product);
+};
+
+
+// @desc    Get All Products
+// @route   GET /api/products
+// @access  Public
+const getProducts = async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+};
+
+
+module.exports = {
+  createProduct,
+  getProducts,
+};
