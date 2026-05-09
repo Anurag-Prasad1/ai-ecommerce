d60e0a60ee0fa1c2a054b4315a7a0f96ebee0be1
Anurag Prasad <anuragprasad309@gmail.com>
2026-05-09 18:54:51 +0530

    feat: added reusable navbar and product card components

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>AI E-commerce 🚀</h2>
+
+      <div>
+        <button>Login</button>
+        <button>Cart</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
