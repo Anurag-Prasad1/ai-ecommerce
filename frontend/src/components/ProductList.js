1fb613ae5707bfa6e2009cd3a02c47461e1e49e7
Anurag Prasad <anuragprasad309@gmail.com>
2026-05-13 22:35:11 +0530

    feat: connected frontend search with backend API

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,21 +1,28 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+
+import { useLocation } from "react-router-dom";
+
 import ProductCard from "./ProductCard";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
 
+  const location = useLocation();
+
+  const keyword = new URLSearchParams(location.search).get("keyword");
+
   useEffect(() => {
     const fetchProducts = async () => {
       const { data } = await axios.get(
-        "http://localhost:5000/api/products"
+        `http://localhost:5000/api/products?keyword=${keyword || ""}`
       );
 
       setProducts(data.products || data);
     };
 
     fetchProducts();
-  }, []);
+  }, [keyword]);
 
   return (
     <div className="product-grid">
