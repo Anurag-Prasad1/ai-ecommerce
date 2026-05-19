f15c62f3b09da59200d647013e56e736617fac0f
Anurag Prasad <anuragprasad309@gmail.com>
2026-05-19 22:31:35 +0530

    feat: added checkout navigation from cart page

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 
+import { Link } from "react-router-dom";
+
 import { CartContext } from "../context/CartContext";
 
 function CartPage() {
@@ -15,31 +17,41 @@ function CartPage() {
       {cartItems.length === 0 ? (
         <h3>Cart is Empty</h3>
       ) : (
-        cartItems.map((item) => (
-          <div
-            key={item._id}
-            className="cart-item"
-          >
-            <h3>{item.name}</h3>
-
-            <p>
-              ₹{" "}
-              {item.price.toLocaleString(
-                "en-IN"
-              )}
-            </p>
-
-            <p>Quantity: {item.qty}</p>
-
-            <button
-              onClick={() =>
-                removeFromCart(item._id)
-              }
+        <>
+          {cartItems.map((item) => (
+            <div
+              key={item._id}
+              className="cart-item"
             >
-              Remove
+              <h3>{item.name}</h3>
+
+              <p>
+                ₹{" "}
+                {item.price.toLocaleString(
+                  "en-IN"
+                )}
+              </p>
+
+              <p>
+                Quantity: {item.qty}
+              </p>
+
+              <button
+                onClick={() =>
+                  removeFromCart(item._id)
+                }
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+
+          <Link to="/shipping">
+            <button>
+              Proceed To Checkout
             </button>
-          </div>
-        ))
+          </Link>
+        </>
       )}
     </div>
   );
