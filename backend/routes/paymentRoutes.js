d4582eae46232cb4dee31f24f2e5d8362c83d912
Anurag Prasad <anuragprasad309@gmail.com>
2026-05-20 16:43:49 +0530

    feat: added payment routes

---
+++ backend/routes/paymentRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  createPaymentOrder,
+} = require("../controllers/paymentController");
+
+router.post(
+  "/create-order",
+  createPaymentOrder
+);
+
+module.exports = router;
