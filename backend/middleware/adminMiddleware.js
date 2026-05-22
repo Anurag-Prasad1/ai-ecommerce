d88a48952394c78abe13666c08ee1ed8f2767200
Anurag Prasad <anuragprasad309@gmail.com>
2026-05-22 21:16:58 +0530

    feat: added admin authorization middleware

---
+++ backend/middleware/adminMiddleware.js
@@ -0,0 +1,15 @@
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+
+    throw new Error(
+      "Not authorized as admin"
+    );
+  }
+};
+
+module.exports = {
+  admin,
+};
