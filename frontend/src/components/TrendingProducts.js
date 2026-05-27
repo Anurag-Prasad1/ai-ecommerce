a88c0bcafc86f5c6123f1dc8ab6e42c940fc0911
Anurag Prasad <anuragprasad309@gmail.com>
2026-05-27 21:02:46 +0530

    feat: added frontend trending products component

---
+++ frontend/src/components/TrendingProducts.js
@@ -0,0 +1,50 @@
+import {
+  useEffect,
+  useState,
+} from "react";
+
+import axios from "axios";
+
+import ProductCard from "./ProductCard";
+
+function TrendingProducts() {
+  const [products, setProducts] =
+    useState([]);
+
+  useEffect(() => {
+    const fetchTrending =
+      async () => {
+        const { data } =
+          await axios.get(
+            "http://localhost:5000/api/products/trending/products"
+          );
+
+        setProducts(data);
+      };
+
+    fetchTrending();
+  }, []);
+
+  return (
+    <div>
+      {products.length > 0 && (
+        <>
+          <h2>
+            🔥 Trending Products
+          </h2>
+
+          <div className="product-grid">
+            {products.map((product) => (
+              <ProductCard
+                key={product._id}
+                product={product}
+              />
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default TrendingProducts;
