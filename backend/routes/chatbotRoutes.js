ac82a84f0e46da2be8b8291ad19be445d83a24c5
Anurag Prasad <anuragprasad309@gmail.com>
2026-05-28 21:02:08 +0530

    feat: added chatbot API routes

---
+++ backend/routes/chatbotRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  chatbotReply,
+} = require("../controllers/chatbotController");
+
+router.post("/", chatbotReply);
+
+module.exports = router;
